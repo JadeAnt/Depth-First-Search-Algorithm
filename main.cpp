71d6d142f173908f9b758a0dbd569c3fb173073e
Jaden Antoine <jadenjantoine@gmail.com>
2019-08-28 20:31:22 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+struct Vertex{
+  char label;
+  bool visited;
+};
+
+//stack functions
+class Stack{
+  public:
+  
+  private:
+    void push();
+    char pop();
+    void top();
+    
+}
+
+//graph functions
