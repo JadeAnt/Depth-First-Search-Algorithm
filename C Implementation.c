72385000b2e5fee38d3dffeb06a1cf1ff718873b
Jaden Antoine <jadenjantoine@gmail.com>
2019-09-03 23:21:06 -0400

    Update C Implementation.c

--- C Implementation.c
+++ C Implementation.c
@@ -2,11 +2,40 @@
 #include <stdlib.h>
 
 //stack functions
+int A[MAX];
+int top = -1;
 
+void push(int data){
+  A[top++] = data;
+}
+
+int pop(){
+  int data;
+  data = A[top--];
+}
+
+int top(){
+  return A[top];
+}
+
+void isEmpty(){
+  if(top == -1){
+    printf("Stack is empty");
+  else
+    printf("Stack is not empty");
+}
 
 
 //graph functions
-
+int edgeList[MAX];
+int adjMatrix[MAX];
+  
+  
+int addEdge();
+  
+int addVerticies();
+  
+int depthFirstSearch();
 
 
 int main(){
